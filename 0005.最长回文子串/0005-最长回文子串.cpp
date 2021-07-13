0070ae120c0bfc404583ff84923ed3fc04cc73a3
LuJie Li <lujieli@LuJiedeMacBook-Pro.local>
2021-07-13 13:05:47 +0800

    2021-07-13

--- 0005.最长回文子串/0005-最长回文子串.cpp
+++ 0005.最长回文子串/0005-最长回文子串.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
+        // dp[i][j] 表示 s[i..j] 是否是回文串
         vector<vector<int>> dp(n, vector<int>(n));
         string ans;
         for (int l = 0; l < n; ++l) {//注意，l和i不要反了
             for (int i = 0; i + l < n; ++i) {
                 int j = i + l;
                 if (l == 0) {
-                    dp[i][j] = 1;
+                    dp[i][j] = 1; // 长度为1的串都是回文串
                 } else if (l == 1) {
-                    dp[i][j] = (s[i] == s[j]);
+                    dp[i][j] = (s[i] == s[j]);  // 长度为2的串，如果两个字母相等，则为回文串
                 } else {
                     dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
                 }
